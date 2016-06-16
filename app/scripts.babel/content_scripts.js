be5cfacf7fd9ce763ce6dc10df8a6cc05dc27dfc
Kien Pham <shop@kienpham.com>
2016-06-15 20:15:10 -0700

    0.0.4

---
+++ app/scripts.babel/content_scripts.js
@@ -0,0 +1,8 @@
+'use strict';
+$(function() {
+    document.onselectionchange = function() {
+        $('.highlightMenu').remove();
+        $('.highlightMenu-inner').remove();
+        $('.highlightMenu-arrowClip').remove();
+    }
+});
